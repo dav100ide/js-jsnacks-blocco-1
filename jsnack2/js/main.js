95540c53390a2a127429056f3d554509aa5df211
dav100ide <oreglio.dev@gmail.com>
2022-10-26 11:27:59 +0200

    piccola correzione pk leggo male la consegna lol

--- jsnack2/js/main.js
+++ jsnack2/js/main.js
@@ -2,9 +2,9 @@ const word1 = window.prompt('inserisci la prima parola');
 const word2 = window.prompt('inserisci la seconda parola');
 const message = document.getElementById('message');
 if (word1.length > word2.length) {
-   message.innerHTML = `la parola "${word1}" è più lunga`;
+   message.innerHTML = `la parola "${word1}" è più lunga quindi stampo: ${word1}${word2}`;
 } else if (word2.length > word1.length) {
-   message.innerHTML = `la parola "${word2}" è più lunga`;
+   message.innerHTML = `la parola "${word2}" è più lunga quindi stampo: ${word2}${word1}`;
 } else {
    message.innerHTML = `sono lunghe uguali`;
-}
+
