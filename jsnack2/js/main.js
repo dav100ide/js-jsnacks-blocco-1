01c335e5a2ef5c35be951e57800760a300573196
dav100ide <oreglio.dev@gmail.com>
2022-10-26 11:01:55 +0200

    aggiunge stile brutto

--- jsnack2/js/main.js
+++ jsnack2/js/main.js
@@ -1,10 +1,10 @@
 const word1 = window.prompt('inserisci la prima parola');
 const word2 = window.prompt('inserisci la seconda parola');
-
+const message = document.getElementById('message');
 if (word1.length > word2.length) {
-   console.log(`la parola "${word1}" è più lunga`);
+   message.innerHTML = `la parola "${word1}" è più lunga`;
 } else if (word2.length > word1.length) {
-   console.log(`la parola "${word2}" è più lunga`);
+   message.innerHTML = `la parola "${word2}" è più lunga`;
 } else {
-   console.log(`sono lunghe uguali`);
+   message.innerHTML = `sono lunghe uguali`;
 }
